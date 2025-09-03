2064e1377dad7d28479eaeadddd802df6ee2bb62
loicfleury <loic.fleury28@gmail.com>
2025-09-03 11:06:17 +0200

    environement DEV en local

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/armatacompagnie/', // chemin Pages = nom du repo
-})
+  // En dev on sert à la racine '/', en build (et preview) on cible GitHub Pages
+  base: command === 'serve' ? '/' : '/armatacompagnie/',
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    open: true
+  }
+}))
