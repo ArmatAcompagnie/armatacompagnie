4150e0aeecceb93809fc8d7985a303d3db9775fc
loicfleury <loic.fleury28@gmail.com>
2025-12-03 12:04:26 +0100

    fix router replace hashrouter with browserrouter in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import TopNav from './components/TopNav'
 import Footer from './components/Footer'
 import Home from './pages/Home'
