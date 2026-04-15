b88de215554994511cbeb268151145242e41e625
loicfleury <loic.fleury28@gmail.com>
2026-04-15 15:54:53 +0200

    docs: feat README

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const site = {
   facebook: 'https://www.facebook.com/Armata.Cie',
   instagram: 'https://www.instagram.com/armata_cie',
   youtube: 'https://www.youtube.com/@compagniearmata8105',
-  logo: 'images/logo/logo-master.png', // place un logo dans public/images/
+  logo: 'images/logo/logo-master02.png', // place un logo dans public/images/
   ogImage: 'images/og.jpg', // 1200x630 recommandé (public/images/og.jpg)
   address: 'chez Mr Jumeau Romain, App 2 Batiment B, 84 rue de la Vallee de l Eure, 28600 Luisant',
   trainingAddress: 'Complexe sportif Marcel Roblot, 4 Rue Raymond Poirier 28600 Luisant',
